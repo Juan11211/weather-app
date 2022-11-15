5b16c40f5a469d3137a6ecac9f5ca7729e2312ac
Juan <101063129+Juan11211@users.noreply.github.com>
2022-11-15 00:08:36 -0500

    first stages of weather app

---
+++ src/HomeScreen.js
@@ -0,0 +1,55 @@
+import React, {useState} from 'react'
+ // import requests from '../request'
+// import axios from '../Axios'
+import axios from 'axios'
+
+function HomeScreen(){
+
+    const [data, setData] = useState({})
+    const [location, setLocation] = useState('')
+    
+    // location is the query
+        const baseUrl = `https://api.openweathermap.org/data/2.5/weather?q=${location}&units=imperial&appid=b30d7792ec0a36d005a1dcfe05f376ed` 
+          
+    
+    // search bar 
+    const searchLocation = (event) => { 
+        if(event.key === 'Enter') { 
+            axios.get(baseUrl)
+        .then((res) => {
+         setData(res.data)
+         console.log(res.data)
+        })
+        }
+    }
+        
+ 
+
+    return (
+        <div className='home'>
+        <div className='container'>
+        <div className='search--bar'>
+            <input 
+                value={location}
+                onChange={event => setLocation(event.target.value)} // 
+                onKeyPress={searchLocation} //
+                placeholder='Enter Location'
+            />
+        </div>
+        </div>
+        <div className='container--card'>
+        <h3 className='temp'>{ data.main ? <p>{data.main.temp.toFixed()}°F </p>: null}</h3>
+        <h1 className='location'>{data.name}</h1>
+        <h3 className='weather'>{data.weather ? <p>{data.weather[0].description}</p> : null}</h3>
+        </div>
+        <div className='container--card--2'>
+        <h3>{ data.main ? <p className='p'>Humidity: {data.main.humidity} </p>: null}</h3>
+        <h3>{ data.main ? <p>Atmospheric pressure: {data.main.pressure} </p>: null}</h3>
+        <h3>{ data.main ? <p>High Temp: {data.main.temp_max.toFixed()} </p>: null}</h3>
+        <h3>{ data.main ? <p>Low Temp: {data.main.temp_min.toFixed()} </p>: null}</h3>
+        </div>
+        </div>
+    )
+}
+
+export default HomeScreen;
