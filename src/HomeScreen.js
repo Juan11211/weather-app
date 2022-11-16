aa38b18e73674e748f45fa66063f20af7a462cf5
Juan <101063129+Juan11211@users.noreply.github.com>
2022-11-16 10:45:22 -0500

    no major changes

--- src/HomeScreen.js
+++ src/HomeScreen.js
@@ -27,7 +27,7 @@ function HomeScreen(){
 
     return (
         <div className='home'>
-        <div className='container'>
+        
         <div className='search--bar'>
             <input 
                 value={location}
@@ -36,17 +36,20 @@ function HomeScreen(){
                 placeholder='Enter Location'
             />
         </div>
-        </div>
         <div className='container--card'>
         <h3 className='temp'>{ data.main ? <p>{data.main.temp.toFixed()}°F </p>: null}</h3>
         <h1 className='location'>{data.name}</h1>
-        <h3 className='weather'>{data.weather ? <p>{data.weather[0].description}</p> : null}</h3>
+        <h3 className='high--temp'>{ data.main ? <p>High Temp: {data.main.temp_max.toFixed()} </p>: null}</h3>
+        <h3 className='low--temp'> { data.main ? <p>Low Temp: {data.main.temp_min.toFixed()} </p>: null}</h3>
+
         </div>
         <div className='container--card--2'>
-        <h3>{ data.main ? <p className='p'>Humidity: {data.main.humidity} </p>: null}</h3>
-        <h3>{ data.main ? <p>Atmospheric pressure: {data.main.pressure} </p>: null}</h3>
-        <h3>{ data.main ? <p>High Temp: {data.main.temp_max.toFixed()} </p>: null}</h3>
-        <h3>{ data.main ? <p>Low Temp: {data.main.temp_min.toFixed()} </p>: null}</h3>
+            <div className='column'>
+        <h3 className='weather'>{data.weather ? <p>{data.weather[0].description}</p> : null}</h3>
+        <h3 className='real--feel'>{ data.main ? <p>Real Feel: {data.main.feels_like.toFixed()}</p>  : null }</h3>
+        <h3 className='humidity'>{ data.main ? <p className='p'>Humidity: {data.main.humidity} </p>: null}</h3>
+        <h3 className='pressure'>{ data.main ? <p>Atmospheric pressure: {data.main.pressure} </p>: null}</h3>
+        </div>
         </div>
         </div>
     )
